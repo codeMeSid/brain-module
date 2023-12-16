a012c68e1507decf9ac4acac60883275ead09c7e
Siddhant <siddhanthbajoria@gmail.com>
2023-12-17 02:15:34 +0530

    GameDirector2 created

---
+++ src/classes/GameDirector2.ts
@@ -0,0 +1,64 @@
+import {
+  Color,
+  GameAssetDimension,
+  GameAssetPosition,
+  GameAssets,
+  GameSettings,
+  ScoringFunc,
+} from "../types";
+
+export abstract class GameDirector2<AssetNameList extends Array<string>> {
+  // game control
+  protected abstract _game_settings: GameSettings;
+  // board
+  protected abstract _board: CanvasRenderingContext2D;
+  // assets
+  protected abstract _game_asset: GameAssets<AssetNameList>;
+  // constructor
+  constructor() {
+    this._init_ = this._init_.bind(this);
+    this._start_ = this._start_.bind(this);
+    this._handleInput_ = this._handleInput_.bind(this);
+    this._checkGameOver_ = this._checkGameOver_.bind(this);
+    this._drawBoard_ = this._drawBoard_.bind(this);
+    this.generateRandomPosition = this.generateRandomPosition.bind(this);
+    this.renderOnBoard = this.renderOnBoard.bind(this);
+  }
+  // game methods
+  public abstract _init_(
+    boardRef: HTMLCanvasElement,
+    scorer: ScoringFunc,
+  ): void; //: GameInitFunc;
+  public abstract _start_(settings?: Partial<GameSettings>): void;
+  protected abstract _handleInput_(ev: KeyboardEvent): void;
+  protected abstract _checkGameOver_(): void;
+  protected abstract _drawBoard_(): void;
+  protected generateRandomPosition = (): GameAssetPosition => ({
+    x: Math.floor(Math.random() * this._game_settings.boardColCount) *
+      this._game_settings.cellSize,
+    y: Math.floor(Math.random() * this._game_settings.boardRowCount) *
+      this._game_settings.cellSize,
+  });
+  protected renderOnBoard = (
+    color: Color,
+    position: GameAssetPosition,
+    dimension: GameAssetDimension,
+    radius: number = 16,
+  ) => {
+    if (!this._board) {
+      console.error("Board Not Recognised");
+      return;
+    }
+    this._board.fillStyle = color;
+    this._board.beginPath();
+    this._board.roundRect(
+      position.x,
+      position.y,
+      dimension.w,
+      dimension.h,
+      radius,
+    );
+    this._board.stroke();
+    this._board.fill();
+  };
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
-export type ScoringEvent = "update" | "reset" | "end" | "add";
+export type Assets = Array<string>;
+
+export type ScoringEvent = "update" | "reset" | "end";
 export type ScoringFunc = (
   scoringEvent: ScoringEvent,
   score: number,
@@ -19,3 +21,24 @@ export type Color = RGB | HEX | string;
 
 export type AssetPosition = { x: number; y: number; z: number };
 export type AssetDimension = { w: number; h: number; d: number };
+
+export type GameAssetPosition = { x: number; y: number };
+export type GameAssetVelocity = { xv: number; yv: number };
+export type GameAssetDimension = { w: number; h: number };
+
+export type GameAssetProperties = {
+  init_position: GameAssetPosition;
+  init_velocity: GameAssetVelocity;
+  color: Color;
+  position: GameAssetPosition;
+  velocity: GameAssetVelocity;
+  metaData: Record<string, unknown>;
+  reset: () => void;
+};
+
+export type GameAssets<AssetNameList extends Array<string>> = {
+  [K in (AssetNameList extends ReadonlyArray<infer U> ? U : never)]:
+    GameAssetProperties;
+};
+
+export type GameStartFunc = (settings?: Partial<GameSettings>) => void;
